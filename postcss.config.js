cd522af58fcb86b9f64f94bdd2a293fe01af1579
Mithu <cemithu06@gmail.com>
2025-05-21 21:44:55 +0600

    Initial commit

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-	plugins: {
-		'@tailwindcss/postcss': {}, // Use the new PostCSS plugin
-		autoprefixer: {},
-	},
-};
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-	return (
-		<div className="bg-blue-500 p-4 text-white">
-			Tailwind Test
-		</div>
-	);
-}
-export default App;
